c77add23ae82bbbd4d87883b55a4bf693b8825a2
Vinicius Arcanjo <viniciusarcanjov@gmail.com>
2017-09-12 14:42:00 -0300

    REST API added

--- README.rst
+++ README.rst
@@ -1,15 +1,24 @@
 kyslacker
 =========
 
-Kytos meets Slack, for your notification/automation needs! This NApp sends Slack notifications via slacker. Essentially, kyslacker is just a wrapper around slacker by leveraging Kytos NApp's messaging queues. As a result, kyslacker provides you a simple and common interface for Slack, which becomes even more important when you have several NApps. Also, kyslacker keeps all Slack dependencies in this package (NApp), so you don't have to keep installing dependencies in other NApps.
+Kytos meets Slack, for your notification/automation needs! This NApp sends Slack notifications via slacker. Essentially, kyslacker is just a wrapper around slacker.
+
+
+You have two interfaces to send messages either via:
+
+1. Kytos NApp's native messaging queue
+2. REST API, '/kytos/kyslacker/send' endpoint
+
+As a result, kyslacker provides you a simple and common interface for Slack, which becomes even more important when you have several NApps and Apps that integrate with your network. Also, kyslacker keeps all Slack dependencies in this package (NApp), so you don't have to keep installing dependencies in other NApps.
 
 
 User Stories
 ============
 
-- Multiple Kytos NApps want to send a Slack message to a specific Slack channel via KytosEvent.
+- Multiple Kytos NApps want to send a message to a specific Slack channel via KytosEvent.
 - Multiple Kytos NApps want to be able add other extra information such as NApp that originated the message, tags, and the content of the Slack message itself.
 - User A wants to have access to one Slack bot.
+- Another App wants to send a message to a specific Slack channel via a REST API.
 
 
 Requirements
@@ -27,12 +36,13 @@ First of all, you need kytos SDN platform up and running and then:
 2. kytos napps install viniciusarcanjo/kyslacker
 3. kytos napps enable viniciusarcanjo/kyslacker
 4. Set your Slack's bot's API token in settings.py. If you don't have a token yet, check this `link <https://my.slack.com/services/new/bot>`_
-5. Start coding your NApp that will send messages (KytosEvent) to kyslacker
 
-Coding your client NApp
-=======================
+Then, you have two options:
 
-The simplest snippet looks like this:
+Option 1 - Coding your client Kytos NApp
+========================================
+
+The simplest snippet looks like this to send messages via KytosEvent:
 
 .. code-block:: python
 
@@ -49,7 +59,7 @@ The simplest snippet looks like this:
       log.info('sending message to kyslacker...')
       self.controller.buffers.app.put(event)
 
-If you want to prototype quickly with the above snippet and you don't have a base NApp yet, I recommend you pull this sample client application that I published on kytos napps repos:
+If you want to prototype quickly with the above snippet and you don't have a base NApp yet, I recommend you pull this sample client `application <https://www.github.com/viniciusarcanjo/kyslacker-client>`_ that I published on kytos napps repos:
 
 .. code-block:: shell
 
@@ -57,6 +67,26 @@ If you want to prototype quickly with the above snippet and you don't have a bas
   kytos napps enable viniciusarcanjo/kyslacker-client
 
 Go to kytos napps dir and start hacking!
+
+Option 2 - REST POST Call
+=========================
+
+If for some reason, you have other APPs that integrate with the network either via other controllers or other external interfaces, you won't be able to leverage KytosEvent, which is why this REST POST endpoint `/kytos/kyslacker/send` is available. To send a message, just send a json on this endpoint:
+
+.. code-block:: json
+
+  {
+    "channel": "of_notifications",
+    "source": "kyslacker-client",
+    "tag": "INFO",
+    "payload": "L2circuit Y was provisioned for customer B"
+  }
+
+If this json data is written on a `msg.json` file you can test it with curl:
+
+.. code-block:: shell
+
+   curl -X POST -d@msg.json -H "Content-Type: application/json" localhost:8181/kytos/kyslacker/send
 
 FAQ
 ===
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import json
+from flask import request
 from kytos.core import KytosEvent, KytosNApp, log
 from kytos.core.helpers import listen_to
 from napps.viniciusarcanjo.kyslacker import settings
@@ -16,13 +18,15 @@ class Main(KytosNApp):
         """
         self.has_failed = False
         try:
-            from slacker import Slacker
+            from slacker import Slacker, Error
             if not settings.token:
                 self.has_failed = True
                 log.error("Please, set the token variable in settings.py")
                 log.error("kyslacker will shut down")
             else:
                 self.slack = Slacker(settings.token)
+                self.error = Error
+                self._register_rest()
         except ImportError as e:
             log.error(
                 "Package slacker inst't installed. Please, pip install slacker"
@@ -35,6 +39,30 @@ class Main(KytosNApp):
         if self.has_failed:
             self.controller.unload_napp(self.username, self.name)
         pass
+
+    def _register_rest(self):
+        """Register REST endpoint
+        POST /kyslacker/send
+
+        The following keys can be used in the json content.
+        The 'payload' is mandatory
+
+        # 'channel' is the slack channel, by default it's #general
+        # 'source' is the App username/name that fired the event
+        # 'tag' is a string to prefix the 'payload'
+        # 'payload' is a string that will be sent
+
+        d = {
+            'channel': 'of_notifications',
+            'source': '{0}/{1}'.format(self.username, self.name),
+            'tag': 'INFO',
+            'payload': 'L2circuit X was provisioned for customer A'
+        }
+
+        """
+        endpoints = [('/kyslacker/send', self.rest_send, ['POST'])]
+        for endpoint in endpoints:
+            self.controller.register_rest_endpoint(*endpoint)
 
     def _parse_str(self, str1, msg):
         """ Check if msg is a string and concatenate to str1
@@ -49,11 +77,50 @@ class Main(KytosNApp):
         # Otherwise just return the base string
         return str1
 
+    def rest_send(self):
+        """REST POST /kyslacker/send
+        """
+        try:
+            ret = self._send(request.get_json())
+            return json.dumps({"response": "message sent"}), 201
+        except ValueError as e:
+            return json.dumps({
+                "response": "failed reason {0}".format(str(e))
+            }), 400
+
+    def _send(self, d):
+        """
+        Sends this 'd' dict via slacker
+        """
+        try:
+            # if settings wasn't properly setup in the first place
+            if not self.has_failed:
+                if not d.get('payload'):
+                    raise ValueError(
+                        "The dictionary should contain at least the 'payload' key-value"
+                    )
+                slack_msg = ""
+                for k in ['source', 'tag', 'payload']:
+                    slack_msg = self._parse_str(slack_msg, d.get(k))
+                # fallback to #general
+                ch = d.get('channel') if d.get('channel') else 'general'
+                log.info('channel:{0} msg:{1}'.format(ch, slack_msg))
+                self.slack.chat.post_message(ch, slack_msg)
+        except requests.exceptions.ConnectionError as e:
+            err = "ConnectionError to Slack API. Make sure you are connected and can reach Slack API."
+            log.error(err)
+            raise ValueError(err)
+        except self.error as e:
+            err = "This slack channel {0} doesn't exist".format(ch)
+            log.error(err)
+            raise ValueError(err)
+
     @listen_to('viniciusarcanjo/kyslacker.send')
     def send(self, event):
         """Listen to KytosEvent 'kyslacker.send' parse it and send it via slacker
 
         Here's a simple snippet to send a event and the expected dict keys:
+        The 'payload' is mandatory
 
         event = KytosEvent(name='viniciusarcanjo/kyslacker.send')
         d = {
@@ -66,38 +133,14 @@ class Main(KytosNApp):
         self.controller.buffers.app.put(event)
 
         # 'channel' is the slack channel, by default it's #general
-        # 'source' is the NApp username/name that fired the event
+        # 'source' is the App username/name that fired the event
         # 'tag' is a string to prefix the 'payload'
         # 'payload' is a string that will be sent
 
         :event: KytosEvent
 
         """
-
-        try:
-            # if settings wasn't properly setup in the first place
-            if not self.has_failed:
-                d = event.content.get('message')
-                if d:
-                    if not isinstance(d, dict):
-                        log.error(
-                            "The object in KytosEvent.content.message isn't a dict!"
-                        )
-                        return
-                    slack_msg = ""
-                    for k in ['source', 'tag', 'payload']:
-                        slack_msg = self._parse_str(slack_msg, d.get(k))
-                    # only sends if it's not empty.
-                    if slack_msg:
-                        # fallback to #general
-                        ch = d.get('channel') if d.get(
-                            'channel') else '#general'
-                        log.info('channel:{0} msg:{1}'.format(ch, slack_msg))
-                        self.slack.chat.post_message(ch, slack_msg)
-        except requests.exceptions.ConnectionError as e:
-            log.error(
-                "ConnectionError to Slack API. Make sure you are connected and can reach Slack API."
-            )
+        self._send(event.content.get('message'))
 
     def shutdown(self):
         """Too simple to have a shutdown procedure."""
